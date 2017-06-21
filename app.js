1ab01e71f23308ea7f2fa778e3177f1792497dec
EamonBachari <eamonbachari@gmail.com>
2017-06-21 12:48:38 -0500

    Add validation features without position validation.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mustacheExpress = require('mustache-express');
 const bodyParser = require('body-parser');
+const expressValidator = require('express-validator')
 const app = express();
 
 app.engine('mustache', mustacheExpress() )
@@ -9,6 +10,7 @@ app.set('view engine', 'mustache');
 
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(expressValidator());
 
 app.listen(3000, function(){
   console.log("Server connected!");
@@ -21,11 +23,34 @@ app.get('/form', function(request, response){
 
 app.post('/thankyou', function(request, response){
 
-  response.render("thankyou", {
-    name: request.body.name,
-    email: request.body.email,
-    birthYear: request.body.birthYear,
-    position: request.body.position,
-    password: request.body.password
-  });
+  request.checkBody("name", "You must enter a name!").notEmpty();
+  request.checkBody("name", "100 characters max.").isLength({min:0, max: 100});
+
+  request.checkBody("email", "You must enter a email! 100 characters max.").notEmpty();
+  request.checkBody("email", "100 characters max.").isLength({min: 0, max: 100});
+
+  request.checkBody("birthYear", "Must be after 1899.").isAfter("1899");
+  request.checkBody("birthYear", "Must be before 2017.").isBefore("2017");
+
+  request.checkBody("position", "You must enter a position!").notEmpty();
+
+  request.checkBody("password", "You must enter a password!").notEmpty();
+  request.checkBody("password", "Password must be at least 8 characters long.").isLength({min:8, max:30});
+
+  var errors = request.validationErrors();
+
+  if(errors){
+    var html = errors;
+    response.send(html);
+  }
+  else {
+    response.render("thankyou", {
+      name: request.body.name,
+      email: request.body.email,
+      birthYear: request.body.birthYear,
+      position: request.body.position,
+      password: request.body.password
+    });
+
+  }
 });
